b93694479613467c1687169688fe2e53bdd00589
Artem Romenskyi <romenskiy.artem@gmail.com>
2023-07-16 16:17:21 +0300

    remove unused classes

--- app/src/main/java/com/somenthingnice/photos/entity/hisoty/HistoryItem.kt
+++
@@ -1,6 +0,0 @@
-package com.somenthingnice.photos.entity.hisoty
-
-data class HistoryItem(
-    val id: Long,
-    val number: Long
-)
--- app/src/main/java/com/somenthingnice/photos/entity/number/NumberInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.somenthingnice.photos.entity.number
-
-data class NumberInfo(
-    val number: Long,
-    val info: String
-)
